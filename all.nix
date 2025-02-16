08e12d9b8bc8b6ea615f8393887d03643ad80def
Luchang Jin <ljin.luchang@gmail.com>
2025-02-16 00:10:19 -0500

    add another test

--- all.nix
+++ all.nix
@@ -11,16 +11,9 @@ let
     nvidiaHash = "sha256-ESBH9WRABWkOdiFBpVtCIZXKa5DvQCSke61MnoGHiKk=";
   });
 
-  versionFile440 = (pkgs.callPackage ./nixGL.nix {
-    nvidiaVersionFile = pkgs.writeText "nvidia-version-440.82" ''
-      NVRM version: NVIDIA UNIX x86_64 Kernel Module  440.82  Wed Apr  1 20:04:33 UTC 2020
-      GCC version:  gcc version 9.3.0 (Arch Linux 9.3.0-1)
-    '';
-  });
-
-  versionFile510 = (pkgs.callPackage ./nixGL.nix {
-    nvidiaVersionFile = pkgs.writeText "nvidia-version-510.54" ''
-      NVRM version: NVIDIA UNIX x86_64 Kernel Module  510.54  Wed Apr  1 20:04:33 UTC 2020
+  versionFile550 = (pkgs.callPackage ./nixGL.nix {
+    nvidiaVersionFile = pkgs.writeText "nvidia-version-550.135" ''
+      NVRM version: NVIDIA UNIX x86_64 Kernel Module  550.135  Wed Apr  1 20:04:33 UTC 2020
       GCC version:  gcc version 9.3.0 (Arch Linux 9.3.0-1)
     '';
   });
@@ -37,8 +30,6 @@ in
   ++
   (with pure550; [nixGLIntel nixVulkanNvidia nixGLNvidia nixVulkanIntel])
   ++
-  # (with versionFile440.auto; [nixGLNvidia nixGLDefault nixVulkanNvidia])
-  # ++
-  # (with versionFile510.auto; [nixGLNvidia nixGLDefault nixVulkanNvidia])
-  # ++
+  (with versionFile550.auto; [nixGLNvidia nixGLDefault nixVulkanNvidia])
+  ++
   (with versionFile565.auto; [nixGLNvidia nixGLDefault nixVulkanNvidia])

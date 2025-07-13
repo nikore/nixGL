ad6c6c2c7dbb629e436056683d567f619c1d9d92
Luchang Jin <ljin.luchang@gmail.com>
2025-07-12 22:06:53 -0400

    update nixpkgs rev

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,5 @@
 let
-  rev = "0b73e36b1962620a8ac551a37229dd8662dac5c8";
+  rev = "10e687235226880ed5e9f33f1ffa71fe60f2638a";
 in
 import (fetchTarball {
   url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
